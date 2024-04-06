8924f75a9c9b04beb1cbe4b91565455ef6c91b47
Tien-Cheng <tiencheng@protonmail.com>
2024-04-06 16:01:29 +0800

    docs: add todo comments

--- backend/routes/discover/index.ts
+++ backend/routes/discover/index.ts
@@ -3,6 +3,12 @@ import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 const main : FastifyPluginAsyncTypebox = async (app, options): Promise<void> => {
     app.get("/", {},
     async (req, res) => {
+       // TODO: autohook discover client
+
+       // TODO: start scan with 10s second timeout? (or can this be SSE?)
+
+
+       // Return list of devices along with connection settings
        return res.send({
             "message" : "TBC",
             "devices" : ["test"]
