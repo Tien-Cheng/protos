c32bbab9eb7b5d94aec8681fd09d9354a1c497ac
Tien-Cheng <tiencheng@protonmail.com>
2024-04-07 06:37:49 +0800

    feat: implement call to add device polling to task queue

--- backend/src/routes/devices/discover/index.ts
+++ backend/src/routes/devices/discover/index.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 import { Timestamp } from "firebase-admin/firestore";
+import { DeviceType } from "types";
 
 const main: FastifyPluginAsyncTypebox = async (app, options): Promise<void> => {
   app.get("/", {}, async (req, res) => {
@@ -14,19 +15,45 @@ const main: FastifyPluginAsyncTypebox = async (app, options): Promise<void> => {
         deviceBrand: device.deviceBrand,
         created: Timestamp.now(),
         isFavourite: false,
-        roomId: "XpCpkNqWYMGACHNw4rJu"
-      }
+        roomId: "XpCpkNqWYMGACHNw4rJu",
+      };
       return data;
-    })
+    });
 
+    await Promise.all(
+      deviceDetails.map(async (device) => {
+        await app.db.collection("Devices").doc(device.deviceId).set(device);
+        // Once added, we need to start collecting data from the device
+        if (device.deviceType == DeviceType.SmartHub) {
+          return;
+        }
 
-    await Promise.all(deviceDetails.map(async (device) => {
-      await app.db.collection("Devices").doc(device.deviceId).set(device);
-    }))
+        const queue =
+          app.tasks.queues["devices"] ?? app.tasks.createQueue(device.deviceId);
+
+        // note this is hacky as if the ip address changes
+        // this will fail
+        const url = device.deviceUrl.split(":");
+        return await queue.add(
+          device.deviceId,
+          {
+            host: url[0],
+            port: url[1],
+            deviceBrand: device.deviceBrand,
+            deviceType: device.deviceType,
+          },
+          {
+            repeat: {
+              every: 5000, // poll every 5 seconds
+            },
+          }
+        );
+      })
+    );
 
     // Return list of devices along with connection settings
     return res.send({
-      deviceDetails
+      deviceDetails,
     });
   });
 };
