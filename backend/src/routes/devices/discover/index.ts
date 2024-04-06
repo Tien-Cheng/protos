5c72b0c80a19be0c1d3faf8a357e49a8c41e6069
Tien-Cheng <tiencheng@protonmail.com>
2024-04-07 05:53:44 +0800

    feat: do both adding devices to db and scanning at the same time

--- backend/src/routes/devices/discover/index.ts
+++ backend/src/routes/devices/discover/index.ts
@@ -1,13 +1,32 @@
 import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import { Timestamp } from "firebase-admin/firestore";
 
 const main: FastifyPluginAsyncTypebox = async (app, options): Promise<void> => {
   app.get("/", {}, async (req, res) => {
     const devices = await app.deviceClient.scan();
     // TODO: start scan with 10s second timeout? (or can this be SSE?)
+    const deviceDetails = devices.map((device) => {
+      const data = {
+        deviceId: device.deviceId,
+        deviceUrl: device.deviceUrl,
+        deviceType: device.deviceType,
+        deviceName: device.deviceName,
+        deviceBrand: device.deviceBrand,
+        created: Timestamp.now(),
+        isFavourite: false,
+        roomId: "XpCpkNqWYMGACHNw4rJu"
+      }
+      return data;
+    })
+
+
+    await Promise.all(deviceDetails.map(async (device) => {
+      await app.db.collection("Devices").doc(device.deviceId).set(device);
+    }))
 
     // Return list of devices along with connection settings
     return res.send({
-      devices
+      deviceDetails
     });
   });
 };
